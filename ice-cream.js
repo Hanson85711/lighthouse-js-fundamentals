83426eca4a05cd8a8adede82832551eeb2112a8a
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-05 10:08:08 +0000

    Ice cream.js file for testing ice cream flavour challenge. packing-list.js for iterating over arrays assignment

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//First Flavour of Array
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours.length);
